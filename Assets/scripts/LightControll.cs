2abc5f179da5ba5cd2029bf49e4115200c1cab77
Dima Shymko <trozeman@gmail.com>
2019-12-14 14:20:39 +0200

    Folders 😎

---
+++ Assets/scripts/LightControll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControll : MonoBehaviour
+{
+
+    public Light _light;
+    // Start is called before the first frame update
+
+    // Timer
+
+    // Intencity hanger 
+
+    // Resizer
+
+    void Start()
+    {
+        _light.range = 20f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
