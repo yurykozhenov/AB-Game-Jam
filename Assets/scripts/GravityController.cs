d0b74b57e887805edcae83c1c1de82f1d70be3c6
Yury Kozhenov <yurykozhenov@gmail.com>
2019-12-15 12:36:24 +0200

    Add gravity controller

---
+++ Assets/scripts/GravityController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    public Transform attractor;
+    public float gravityScale;
+    
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate()
+    {
+        rb.position = Vector3.MoveTowards(rb.position, attractor.position, gravityScale * Time.deltaTime);
+    }
+}
